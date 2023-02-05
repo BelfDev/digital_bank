8779ab7b075efcb1c75a10e6286b97e76e4c37b1
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-05 19:06:25 +0100

    Create Environment config

--- packages/configs/lib/configs.dart
+++ packages/configs/lib/configs.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library configs;
 
-export 'src/configs_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/model/environment.dart' show Environment;
---
+++ packages/configs/lib/src/model/environment.dart
@@ -0,0 +1,7 @@
+enum Environment {
+  production('https://interview-backend-3enbou53tq-uc.a.run.app');
+
+  const Environment(this.flowBankApiBaseUrl);
+
+  final String flowBankApiBaseUrl;
+}
