84ad8db788823248d3e3763ac23609b7f943b987
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 00:29:31 +0100

    Add decrypt

--- packages/data_access/lib/src/service/encryption/encryption_service.dart
+++ packages/data_access/lib/src/service/encryption/encryption_service.dart
@@ -1,8 +1,15 @@
 import 'dart:convert';
 
+import 'package:data_access/src/model/encrypted_data.dart';
+
 mixin EncryptionService {
   String encrypt(Object? object) {
     final encodedJsonPayload = jsonEncode(object);
     return 'encrypted_data';
+  }
+
+  String decrypt(EncryptedData encryptedData) {
+    final decryptedJson = '{"accountNumber":"5339460"}';
+    return decryptedJson;
   }
 }
