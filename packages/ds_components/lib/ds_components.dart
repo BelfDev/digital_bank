e667f9f858325e2c932fb024e55c423defb1e740
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-05 16:57:09 +0100

    Create package for design system components

---
+++ packages/ds_components/lib/ds_components.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library ds_components;
+
+export 'src/ds_components_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
