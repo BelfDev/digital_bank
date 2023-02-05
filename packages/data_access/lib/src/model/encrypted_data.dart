a23fe9bd9adea04be9763313e724e2ef9beca77c
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-05 18:33:29 +0100

    Create EncryptedData

---
+++ packages/data_access/lib/src/model/encrypted_data.dart
@@ -0,0 +1,24 @@
+import 'package:meta/meta.dart';
+
+import 'encryption_protocol.dart';
+
+@immutable
+class EncryptedData<T> implements EncryptionProtocol<T> {
+  const EncryptedData({
+    required this.payload,
+  });
+
+  final T payload;
+
+  @override
+  T decrypt(String input) {
+    // TODO: implement decrypt
+    throw UnimplementedError();
+  }
+
+  @override
+  String encrypt(T input) {
+    // TODO: implement encrypt
+    throw UnimplementedError();
+  }
+}
