62c088f69c0174445e0d511561cd688580ef185e
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 20:46:44 +0100

    Remove legacy code

--- packages/ds_components/test/ds_components_test.dart
+++ packages/ds_components/test/ds_components_test.dart
@@ -3,14 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
     setUp(() {
       // Additional setup goes here.
     });
 
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(true, isTrue);
     });
   });
 }
