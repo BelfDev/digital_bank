9ab9efe531307fb8244b078f9e3a2ac757280aab
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 18:59:24 +0100

    Create welcome feature package

---
+++ features/welcome/test/welcome_test.dart
@@ -0,0 +1,16 @@
+import 'package:welcome/welcome.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
