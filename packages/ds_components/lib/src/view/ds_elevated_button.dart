89d02fc8b7809f5ef21bfcf6095dfa48b92a1397
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 00:00:13 +0100

    Add DSElevatedButton theme

--- packages/ds_components/lib/src/view/ds_elevated_button.dart
+++ packages/ds_components/lib/src/view/ds_elevated_button.dart
@@ -1,3 +1,4 @@
+import 'package:ds_components/ds_components.dart';
 import 'package:flutter/material.dart';
 
 import '../theme/ds_gradients.dart';
@@ -8,34 +9,33 @@ class DSElevatedButton extends StatelessWidget {
     required this.onPressed,
     required this.text,
     this.width,
-    this.height = 64.0,
+    this.height,
     this.gradient = DSGradients.blueShades,
   });
 
   final VoidCallback? onPressed;
   final String text;
   final double? width;
-  final double height;
+  final double? height;
   final Gradient gradient;
 
   @override
   Widget build(BuildContext context) {
-    const borderRadius = BorderRadius.all(Radius.circular(16.0));
+    final elevatedButtonStyle = Theme.of(context).elevatedButtonTheme.style;
+    final effectiveHeight = height ??
+        elevatedButtonStyle?.minimumSize
+            ?.resolve({MaterialState.focused})?.height;
 
     return Container(
       width: width,
-      height: height,
+      height: effectiveHeight,
       decoration: BoxDecoration(
         gradient: gradient,
-        borderRadius: borderRadius,
+        borderRadius: DSTheme.elevatedButtonBorderRadius,
       ),
       child: ElevatedButton(
         onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          shape: RoundedRectangleBorder(borderRadius: borderRadius),
-        ),
+        style: elevatedButtonStyle,
         child: Text(text),
       ),
     );
