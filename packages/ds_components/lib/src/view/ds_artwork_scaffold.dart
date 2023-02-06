ee20b8fe4b15f9088016ad559ea71b91cfdf8d8c
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 22:38:18 +0100

    Create DSArtworkScaffold

---
+++ packages/ds_components/lib/src/view/ds_artwork_scaffold.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import 'ds_artwork_image.dart';
+
+class DSArtworkScaffold extends StatelessWidget {
+  const DSArtworkScaffold({
+    super.key,
+    this.appBar,
+    required this.body,
+    this.imageProvider,
+    this.floatingButton,
+    this.padding,
+  });
+
+  /// An app bar to display at the top of the scaffold.
+  final PreferredSizeWidget? appBar;
+
+  /// The primary content of the scaffold.
+  final Widget body;
+
+  /// The image to display.
+  final ImageProvider? imageProvider;
+
+  /// A button displayed floating above [body], aligned to the bottom center.
+  final Widget? floatingButton;
+
+  /// The amount of space by which to inset the body.
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    final effectiveButton = floatingButton != null
+        ? Padding(padding: padding ?? EdgeInsets.zero, child: floatingButton!)
+        : null;
+
+    return Scaffold(
+      appBar: appBar,
+      body: SingleChildScrollView(
+        padding: padding,
+        child: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              if (imageProvider != null) DSArtworkImage(imageProvider!),
+              const SizedBox(height: 32.0),
+              body,
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: effectiveButton,
+    );
+  }
+}
