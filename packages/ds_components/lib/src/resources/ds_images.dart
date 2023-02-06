98ceb43a824ba3771465fcb7de4f070c266ef271
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 20:46:07 +0100

    Add image assets

---
+++ packages/ds_components/lib/src/resources/ds_images.dart
@@ -0,0 +1,10 @@
+/// Utility class that exposes local application images.
+class DSImages {
+  const DSImages._();
+
+  static const _imagesPath = 'assets/images';
+
+  static const String applicationInProgress =
+      '$_imagesPath/application_illustration.png';
+  static const String bankingApp = '$_imagesPath/banking_app_illustration.png';
+}
