d2ce80e2481cd6e2c94753bcd9b9d49589627d83
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 23:59:35 +0100

    Fix DSArtworkImage constraints

--- packages/ds_components/lib/src/view/ds_artwork_image.dart
+++ packages/ds_components/lib/src/view/ds_artwork_image.dart
@@ -12,13 +12,13 @@ class DSArtworkImage extends StatelessWidget {
   Widget build(BuildContext context) {
     final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
     final effectiveImage = ResizeImage.resizeIfNeeded(
-        (390 * devicePixelRatio).round(), null, imageProvider);
+        (400 * devicePixelRatio).round(), null, imageProvider);
 
     return AspectRatio(
-      aspectRatio: 390.0 / 377.0,
+      aspectRatio: 336.0 / 377.0,
       child: Image(
         image: effectiveImage,
-        fit: BoxFit.contain,
+        fit: BoxFit.cover,
       ),
     );
   }
