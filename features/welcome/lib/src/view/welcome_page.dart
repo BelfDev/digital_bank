81726b5fa8a655bb6aabab3bcea4ee83b5facfc4
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 22:41:19 +0100

    Introduce WelcomePage composition

--- features/welcome/lib/src/view/welcome_page.dart
+++ features/welcome/lib/src/view/welcome_page.dart
@@ -1,3 +1,4 @@
+import 'package:ds_components/ds_components.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -7,12 +8,17 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Welcome'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the app!'),
+    return DSArtworkScaffold(
+      // appBar: AppBar(
+      //   title: const Text('Welcome'),
+      // ),
+      imageProvider: const AssetImage(DSImages.bankingApp),
+      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+      body: Text('''Your Money,\nYour Way,\nDigital.'''),
+      floatingButton: DSElevatedButton(
+        width: double.infinity,
+        onPressed: () {},
+        text: 'start application',
       ),
     );
   }
