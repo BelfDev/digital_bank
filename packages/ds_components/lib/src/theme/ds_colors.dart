7c76dcbdd8a9ad35d9eb7c815925295074e87e77
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 22:39:00 +0100

    Introduce AppColors

---
+++ packages/ds_components/lib/src/theme/ds_colors.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// A collection of static [Color] constants.
+class AppColors {
+  AppColors._();
+
+  static const cyan = Color(0xFF00D4FF);
+  static const vividGreen = Color(0xFF00F8AC);
+  static const green = Color(0xFF78E1A3);
+  static const brightTurquoise = Color(0xFF45E9DE);
+  static const turquoise = Color(0xFF11E2D3);
+  static const persianGreen = Color(0xFF00A8A2);
+  static const pictonBlue = Color(0xFF40D2EF);
+  static const blue = Color(0xFF00B9FF);
+  static const orange = Color(0xFFFF8000);
+  static const salmon = Color(0xFFFF7B7B);
+  static const pink = Color(0xFFFF5897);
+  static const purple = Color(0xFFC367FF);
+  static const vividPurple = Color(0xFFCB97FF);
+
+  static const white = Color(0xFFFFFFFF);
+  static const smoothWhite = Color(0xFFFDFDFD);
+  static const paleWhite = Color(0xFFFCFCFC);
+
+  static const paleBlue = Color(0xFFD7EAF2);
+  static const paleGrey = Color(0xFFF6F6F6);
+  static const handleGrey = Color(0xFFDFDFDF);
+  static const beigeGrey = Color(0xFFEBEBEB);
+  static const lightGrey = Color(0xFFD0E1E8);
+  static const blueGrey = Color(0xFFAABFC8);
+  static const silverGrey = Color(0xFF9AAFB8);
+  static const titaniumGrey = Color(0xFF86979F);
+  static const grey = Color(0xFF6A787C);
+  static const darkGrey = Color(0xFF728187);
+  static const spaceGrey = Color(0xFF677679);
+
+  static const lightBrown = Color(0xFF575757);
+  static const brown = Color(0xFF4D4D4D);
+
+  static const paleBlack = Color(0xFF323E44);
+  static const spaceBlack = Color(0xFF263238);
+  static const mirageBlack = Color(0xFF1B252A);
+
+  static const idleShadow = Color(0x1A000000);
+  static const pressedShadow = Color(0x33000000);
+
+  static const appBarShadow = Color(0x1A263238);
+  static const transparent = Color(0x00000000);
+  static const blueShadow = Color(0x0F263238);
+  static const blueBackdrop = Color(0xD9263238);
+  static const smoothBlueBackdrop = Color(0x99263238);
+
+  // Dark Theme
+  static const smoothBlueGrey = Color(0xFF2C3438);
+
+  static const blueBlack = Color(0xFF151C20);
+  static const smoothBlueBlack = Color(0xFF192126);
+}
