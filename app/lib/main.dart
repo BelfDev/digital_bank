113b574f74f630722d2511d087d4f8f9e4a8db20
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 00:00:44 +0100

    Make the app fixed in portrait mode

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:configs/configs.dart';
 import 'package:ds_components/ds_components.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:welcome/welcome.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: DSColors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: DSColors.grey,
+    ),
+  );
+
   Environment.production.init();
   runApp(const MyApp());
 }
@@ -17,7 +27,9 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: DSTheme.light().data,
       darkTheme: DSTheme.dark().data,
-      home: const WelcomePage(),
+      home: WelcomePage(
+        onPressed: () {},
+      ),
     );
   }
 }
