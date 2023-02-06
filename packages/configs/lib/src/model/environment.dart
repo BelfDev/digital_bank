93be5d174c572f8f83e555ecc53a231e3e740397
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 16:51:36 +0100

    Add environment configurations

--- packages/configs/lib/src/model/environment.dart
+++ packages/configs/lib/src/model/environment.dart
@@ -1,7 +1,46 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'encryption_config.dart';
+
 enum Environment {
-  production('https://interview-backend-3enbou53tq-uc.a.run.app');
+  production;
 
-  const Environment(this.flowBankApiBaseUrl);
+  const Environment();
 
-  final String flowBankApiBaseUrl;
+  Future<void> init() async {
+    await dotenv.load(fileName: ".env");
+  }
+
+  static Environment get current => Environment.production;
+
+  String get flowBankApiBaseUrl {
+    _assertInitialized();
+
+    switch (this) {
+      case Environment.production:
+        return dotenv.env['API_BASE_URL_PROD']!;
+    }
+  }
+
+  EncryptionConfig get encryptionConfig {
+    _assertInitialized();
+
+    switch (this) {
+      case Environment.production:
+        return EncryptionConfig(
+          secretKey: dotenv.env['CRYPTO_SECRET_KEY']!,
+          salt: dotenv.env['CRYPTO_SECRET_SALT']!,
+          iv: dotenv.env['CRYPTO_SECRET_IV']!,
+          iterations: 1000,
+          keyBits: 128,
+        );
+    }
+  }
+
+  void _assertInitialized() {
+    assert(
+      dotenv.isInitialized,
+      'Please ensure the environment is initialized before accessing it.',
+    );
+  }
 }
