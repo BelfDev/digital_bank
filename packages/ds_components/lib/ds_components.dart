b3d4003a4e52761f5eb96bf17fdd7037d605bc49
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-06 23:59:56 +0100

    Expose public_themes.dart

--- packages/ds_components/lib/ds_components.dart
+++ packages/ds_components/lib/ds_components.dart
@@ -1,5 +1,5 @@
 library ds_components;
 
 export 'src/resources/ds_images.dart' show DSImages;
-export 'src/theme/ds_theme.dart' show DSTheme;
+export 'src/theme/public_themes.dart';
 export 'src/view/public_views.dart';
