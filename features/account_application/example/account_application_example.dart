b1701b8a49947aba405ea78d7876798c212f3e09
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 09:07:27 +0100

    Introduce ApplicationFeedbackPage

--- features/account_application/example/account_application_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:account_application/account_application.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
---
+++ features/account_application/lib/src/application_feedback/view/application_feedback_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ApplicationFeedbackPage extends StatelessWidget {
+  const ApplicationFeedbackPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
