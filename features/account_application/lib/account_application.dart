73e962dadf03b6b909f66e6ed79d03e5ffb9ef88
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 10:38:03 +0100

    Clean up code

--- features/account_application/lib/account_application.dart
+++ features/account_application/lib/account_application.dart
@@ -1,8 +1,4 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library account_application;
 
-export 'src/account_application_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/application_feedback/controller/application_feedback_page_controller.dart'
+    show ApplicationFeedbackPageController;
