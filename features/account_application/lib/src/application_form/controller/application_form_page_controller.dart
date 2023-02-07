70f60a64289a8a46fd9e2a9f90a3e4d6f8cd33be
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 11:29:25 +0100

    Introduce application_form_page_controller.dart

---
+++ features/account_application/lib/src/application_form/controller/application_form_page_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ApplicationFormPageController extends StatelessWidget {
+  const ApplicationFormPageController({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
