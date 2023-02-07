0f2810d5885d5b3ec719ee6e9194ce9a6175ec47
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 09:07:19 +0100

    Introduce ApplicationFeedbackPage

---
+++ features/account_application/lib/src/application_feedback/controller/application_feedback_page_controller.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ApplicationFeedbackPageController extends StatelessWidget {
+  const ApplicationFeedbackPageController({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
