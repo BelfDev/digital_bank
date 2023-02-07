e7dc26b2199181cd79f71209c6668f577a50c0f3
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 10:38:14 +0100

    Remove legacy files

--- features/account_application/test/account_application_test.dart
+++ features/account_application/test/account_application_test.dart
@@ -1,16 +1,10 @@
-import 'package:account_application/account_application.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// TODO(BelfDev): Write tests
 void main() {
   group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
     test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+      expect(true, isTrue);
     });
   });
 }
