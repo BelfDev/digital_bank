7259b3b3ddc5a88eda7873903595b15eb5c2e712
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-07 10:38:27 +0100

    Create DSCloseButton

---
+++ packages/ds_components/lib/src/view/ds_close_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../../ds_components.dart';
+
+/// A Material Design close button.
+class DSCloseButton extends StatelessWidget {
+  /// Creates a Material Design close button.
+  const DSCloseButton({super.key, this.color, this.onPressed});
+
+  /// The color to use for the icon.
+  final Color? color;
+
+  /// An override callback to perform instead of the default behavior which is
+  /// to pop the [Navigator].
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    assert(debugCheckHasMaterialLocalizations(context));
+    return IconButton(
+      icon: const Icon(Icons.close),
+      color: color,
+      tooltip: MaterialLocalizations.of(context).closeButtonTooltip,
+      splashRadius: DSTheme.iconSplashRadius,
+      onPressed: () {
+        if (onPressed != null) {
+          onPressed!();
+        } else {
+          Navigator.maybePop(context);
+        }
+      },
+    );
+  }
+}
