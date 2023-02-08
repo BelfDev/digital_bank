8aaaa171fe5ed70c3500f15eeefd92f34fcaff94
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-08 14:06:36 +0100

    Add the dynamic dependent input field

--- features/account_application/lib/src/application_form/view/application_form_page.dart
+++ features/account_application/lib/src/application_form/view/application_form_page.dart
@@ -1,6 +1,8 @@
 import 'package:ds_components/ds_components.dart';
 import 'package:flutter/material.dart';
 
+import 'dependents_dynamic_list_mixin.dart';
+
 class ApplicationFormPage extends StatefulWidget {
   const ApplicationFormPage({super.key});
 
@@ -8,13 +10,9 @@ class ApplicationFormPage extends StatefulWidget {
   State<ApplicationFormPage> createState() => _ApplicationFormPageState();
 }
 
-class _ApplicationFormPageState extends State<ApplicationFormPage> {
+class _ApplicationFormPageState extends State<ApplicationFormPage>
+    with DynamicDependentListMixin {
   final _formKey = GlobalKey<FormState>();
-
-  final List<Widget> _dependentInputFields = [];
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey();
-
-  static const _defaultFormSpacing = 24.0;
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +26,7 @@ class _ApplicationFormPageState extends State<ApplicationFormPage> {
       bodyChildren: [
         DSFormSection(
           title: 'Basic Information',
-          childrenSpacing: _defaultFormSpacing,
+          childrenSpacing: DSTheme.defaultFormSpacing,
           children: [
             DSPhotoInput(
               hint: 'Your picture\n(liveliness check)',
@@ -53,41 +51,23 @@ class _ApplicationFormPageState extends State<ApplicationFormPage> {
           title: 'Dependents information',
           childrenSpacing: 0.0,
           children: [
-            const SizedBox(height: _defaultFormSpacing),
-            // ..._dependentInputFields,
+            const SizedBox(height: DSTheme.defaultFormSpacing),
             AnimatedList(
-              key: _listKey,
+              key: listKey,
               physics: NeverScrollableScrollPhysics(),
               shrinkWrap: true,
-              initialItemCount: _dependentInputFields.length,
-              itemBuilder: (context, index, animation) {
-                return _buildItem(_dependentInputFields[index], animation);
+              initialItemCount: dependentInputFields.length,
+              itemBuilder: (_, index, animation) {
+                return buildDependentTextInputItem(
+                  dependentInputFields[index],
+                  animation,
+                );
               },
             ),
             DSOutlinedButton(
               text: 'add dependent',
               onPressed: () {
-                _insertSingleItem();
-                // setState(() {
-                //   _dependentInputFields.add(
-                //     DSTextInput(
-                //       hintText: 'Last name',
-                //       validator: (value) {
-                //         return null;
-                //       },
-                //     ),
-                //   );
-                // });
-
-                // DSDecisionBottomSheet.present(
-                //   context,
-                //   headerTitle: 'Are you sure you want to remove John Doe?',
-                //   message: 'This action is irreversible.',
-                //   onConfirm: () {
-                //     print('confirmed');
-                //   },
-                //   confirmationText: 'remove',
-                // );
+                spawnDependentTextInput();
               },
             ),
             const SizedBox(height: 120.0),
@@ -100,42 +80,5 @@ class _ApplicationFormPageState extends State<ApplicationFormPage> {
         text: 'submit application',
       ),
     );
-  }
-
-  Widget _buildItem(Widget field, Animation<double> animation) {
-    return SizeTransition(
-      sizeFactor: animation,
-      child: field,
-    );
-  }
-
-  void _insertSingleItem() {
-    int insertIndex = _dependentInputFields.length;
-
-    final key = ValueKey(insertIndex);
-    final newField = DSTextInput(
-      key: key,
-      margin: const EdgeInsets.only(bottom: _defaultFormSpacing),
-      hintText: 'Dependent Fullname',
-      onRemove: () {
-        final index = _dependentInputFields.indexWhere(
-          (element) => element.key == key,
-        );
-        _removeField(index);
-      },
-      validator: (value) {
-        return null;
-      },
-    );
-
-    _dependentInputFields.add(newField);
-    _listKey.currentState?.insertItem(insertIndex);
-  }
-
-  void _removeField(int index) {
-    final removedItem = _dependentInputFields.removeAt(index);
-    _listKey.currentState?.removeItem(index, (_, animation) {
-      return _buildItem(removedItem, animation);
-    });
   }
 }
