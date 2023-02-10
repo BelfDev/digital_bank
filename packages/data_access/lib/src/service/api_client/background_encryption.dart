4b9dc81efb6c49a16d7325a17402ed8e406da07b
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-10 22:25:27 +0100

    Create BackgroundEncryption

---
+++ packages/data_access/lib/src/service/api_client/background_encryption.dart
@@ -0,0 +1,29 @@
+import 'dart:isolate';
+
+import 'package:configs/configs.dart';
+import 'package:data_access/src/service/encryption/encryption_service.dart';
+
+import '../../model/encrypted_data.dart';
+
+class BackgroundEncryption<T> with EncryptionService {
+  const BackgroundEncryption(
+    this._unencryptedData,
+    this._encryptionConfig,
+  );
+
+  final T _unencryptedData;
+  final EncryptionConfig _encryptionConfig;
+
+  Future<EncryptedData> encryptInBackground() async {
+    final p = ReceivePort();
+    await Isolate.spawn(_encrypt, p.sendPort);
+
+    return await p.first;
+  }
+
+  Future<void> _encrypt(SendPort p) async {
+    final encryptedPayload = await encrypt(_unencryptedData, _encryptionConfig);
+    final encryptedData = EncryptedData(payload: encryptedPayload);
+    Isolate.exit(p, encryptedData);
+  }
+}
