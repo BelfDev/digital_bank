e83290cbe493cadc51fa5f04a6c4576bf45acdb1
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-11 00:07:21 +0100

    Add input field validation rules

--- features/account_application/lib/src/application_form/model/form_validator_use_case.dart
+++ features/account_application/lib/src/application_form/model/form_validator_use_case.dart
@@ -7,22 +7,32 @@ class FormValidatorUserCase {
 
   String? validateNameInput(String? value) {
     if (value?.isEmpty ?? true) {
-      return 'value cannot be empty';
+      return _Strings.emptyValueFeedback;
     }
     return null;
   }
 
   String? validateBirthDateInput(String? value) {
     if (value?.isEmpty ?? true) {
-      return 'value cannot be empty';
+      return _Strings.emptyValueFeedback;
+    }
+
+    final parsedDate = DateTime.tryParse(value!);
+    if (parsedDate == null) {
+      return _Strings.invalidFeedback;
     }
     return null;
   }
 
   String? validateGenderInput(GenderOption? value) {
     if (value == null) {
-      return 'value cannot be empty';
+      return _Strings.emptyValueFeedback;
     }
     return null;
   }
+}
+
+class _Strings {
+  static const emptyValueFeedback = 'value cannot be empty';
+  static const invalidFeedback = 'invalid date';
 }
