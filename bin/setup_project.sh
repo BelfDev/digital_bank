18b09f0f94a01c6ae81ac52cdd5e987a3a53d061
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-11 20:25:11 +0100

    Create setup script

---
+++ bin/setup_project.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+echo "\n==========================="
+echo "👋 Welcome to the Setup Script"
+echo "===========================\n"
+echo "Before we begin, we need to make sure that Flutter and Dart are installed on your machine.\n"
+
+## Dependency check
+
+# $1 => Dependency name
+# $2 => Installation function
+suggestInstallation() {
+  echo "⚠️ Something is wrong with $1.\n"
+  while true; do
+      read -p "Do you wish to install $1?" yn
+      case $yn in
+          [Yy]* ) $2; break;;
+          [Nn]* ) exit;;
+          * ) echo "Please answer yes or no. [Y/N]\n";;
+      esac
+  done
+}
+
+showFlutterErrorFeedback() {
+  echo "⚠️ Something is wrong with Flutter.\n"
+  echo "Please visit https://flutter.dev/docs/get-started/install and make sure Flutter 3.7.1 is installed on your machine."
+  exit
+}
+
+flutter --version || showFlutterErrorFeedback
+echo "⭐ All good with Flutter.\n\n"
+
+echo "🚀 Now we're about to install some helper tooling...\n\n"
+
+echo "Setting up Melos..."
+dart pub global activate melos 1>/dev/null
+echo "\n✅ Nice, you can work with a multi-package project\n\n"
+
+
+echo "Installing all dependencies and localizations..."
+dart melos bootstrap 1>/dev/null
+echo "\n✅ Nice, you are ready to run the project!\n\n"
+
+echo "\n======="
+echo "✅ Done"
+echo "=======\n"
