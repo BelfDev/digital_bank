527fb38f64fa7fa463439aa738ab7f71cf98100e
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-11 21:35:25 +0100

    Create test_helper package

---
+++ packages/test_helper/example/test_helper_example.dart
@@ -0,0 +1,6 @@
+import 'package:test_helper/test_helper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/test_helper/test/test_helper_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// TODO(BelfDev): Write tests for test_helper
+void main() {
+  group('A group of tests', () {
+    test('First Test', () {
+      expect(true, isTrue);
+    });
+  });
+}
