bac275303c78ce67783194b69a887eba3b399498
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-11 15:43:15 +0100

    Add application code copied feedback

--- features/account_application/lib/src/application_feedback/controller/application_feedback_page_controller.dart
+++ features/account_application/lib/src/application_feedback/controller/application_feedback_page_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:localization/localization.dart';
 
 import '../view/application_feedback_page.dart';
 
@@ -26,6 +27,7 @@ class ApplicationFeedbackPageController extends StatelessWidget {
     BuildContext context,
     String code,
   ) async {
+    final l10n = L10n.of(context);
     await Clipboard.setData(
       ClipboardData(text: code),
     );
@@ -34,13 +36,9 @@ class ApplicationFeedbackPageController extends StatelessWidget {
         content: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.all(8.0),
-          child: Text(_Strings.copyFeedback),
+          child: Text(l10n.applicationFeedbackCopiedCode),
         ),
       ),
     );
   }
-}
-
-class _Strings {
-  static const copyFeedback = 'copied application code';
 }
