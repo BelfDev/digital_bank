97983785e85346eee88585c89a37817581268d94
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-11 15:49:56 +0100

    Finish iOS localization configuration

--- features/account_application/lib/src/application_form/controller/application_form_page_controller.dart
+++ features/account_application/lib/src/application_form/controller/application_form_page_controller.dart
@@ -4,6 +4,7 @@ import 'package:ds_components/ds_components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:localization/localization.dart';
 
 import '../state_management/application_form_page_state.dart';
 
@@ -42,6 +43,7 @@ class ApplicationFormPageController extends StatelessWidget {
     }
 
     if (next.hasError) {
+      final l10n = L10n.of(context);
       DSErrorFeedbackSnackBar.present(
         context,
         errorFeedback: next.errorFeedback!,
