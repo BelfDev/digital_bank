14462caba32de744d8ccc448cbbf04b51e6134dd
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-12 12:38:09 +0100

    Add background decryption test

--- packages/data_access/test/service/encryption/background_decryption_test.dart
+++ packages/data_access/test/service/encryption/background_decryption_test.dart
@@ -1,11 +1,12 @@
+import 'dart:convert';
+
 import 'package:configs/configs.dart';
 import 'package:data_access/data_access.dart';
-import 'package:data_access/src/model/encrypted_data.dart';
-import 'package:data_access/src/service/encryption/background_encryption.dart';
+import 'package:data_access/src/service/encryption/background_decryption.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('BackgroundEncryption:', () {
+  group('BackgroundDecryption:', () {
     late Environment environment;
 
     setUpAll(
@@ -20,15 +21,16 @@ void main() {
       () async {
         const encryptedPayload =
             '2kT1OKWcpWrPv5qJTtFueMdPRax2AReYrZ8/3SpnGFh1lU8dPWYihWLB1MvFW2PTs/zEPOwiKDxGchY4TzlAkGgCJzbUnndtcAhgkweNq+82OIcv8SFW4lvjWx2fRKleMnhev0tNJMUQxlwBNrW1hAwA65+TvLbbS+TMzHTheHbVPVZSTBvFOlPQ97GIHVG6tZWlyO1OFYJRyGV7uN91+yMmaGs3kDTxqT6REty9vXkmCFw1BDKPccViNMLQ/iSBTD5StafilYVW8iSEK/urdjjd/l5xOwDDuL26cExg7+TvWdfjQZG1MnnhPFtxnC/v';
-
-        final result = await BackgroundEncryption(
-          AccountApplicationStubs.defaultStub,
+        final result = await BackgroundDecryption(
+          jsonEncode({
+            'encryptedResponse': encryptedPayload,
+          }),
           environment.encryptionConfig,
-        ).encryptInBackground();
+        ).decryptInBackground();
 
         expect(
           result,
-          equals(EncryptedData(payload: encryptedPayload)),
+          equals(jsonEncode(AccountApplicationStubs.defaultStub)),
         );
       },
     );
