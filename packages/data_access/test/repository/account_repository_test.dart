de2d485d2fa049ff75826eff128b1b1a32dc1a15
Pedro Belfort <pedro.belfort@lutick.com>
2023-02-13 03:21:55 +0100

    Fix AccountRepository test

--- packages/data_access/test/repository/account_repository_test.dart
+++ packages/data_access/test/repository/account_repository_test.dart
@@ -156,7 +156,7 @@ void main() {
 
         expect(
           provider,
-          isA<Provider<AccountRepository>>(),
+          isA<Provider<AccountRepositoryProtocol>>(),
         );
       },
     );
