35be0c322664c4069070bd915b5c20b60939a81b
Pedro Belfort <pedro.belfort@lutick.com>
2023-04-01 11:48:28 +0200

    Fix all tests

--- packages/configs/test/model/environment_test.dart
+++ packages/configs/test/model/environment_test.dart
@@ -2,11 +2,19 @@ import 'package:configs/configs.dart';
 import 'package:test/test.dart';
 
 void main() {
+  late Environment environment;
+  setUpAll(
+    () async {
+      environment = Environment.test;
+      await environment.init();
+    },
+  );
+
   group('Environment:', () {
-    test('given production outputs expected flowBankApiBaseUrl', () {
+    test('given environment outputs expected flowBankApiBaseUrl', () {
       expect(
-        Environment.production.flowBankApiBaseUrl,
-        equals('https://interview-backend-3enbou53tq-uc.a.run.app'),
+        environment.flowBankApiBaseUrl,
+        equals('https://api.flowbank.com/v1'),
       );
     });
   });
---
+++ packages/localization/test/placeholder_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('Placeholder Test', () {
+    expect(true, isTrue);
+  });
+}
